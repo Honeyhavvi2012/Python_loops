f4b41944effa7649a44ca31db4845e9a97bffc02
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-05-21 11:42:00 +0530

    Add files via upload

---
+++ ASCII Values 2.py
@@ -0,0 +1,8 @@
+print("Uppercase Letters and their ASCII codes:")
+for letter in range(ord('A'), ord('Z') + 1):
+    print(chr(letter), ":", letter)
+
+print("\nLowercase Letters and their ASCII codes:")
+
+for letter in range(ord('a'), ord('z') + 1):
+    print(chr(letter), ":", letter)
